6484077ad21f9e91a289b9b041d20f1204928856
LordEtrius <marciomaia@gmail.com>
2023-01-30 21:35:38 -0300

    JG player move
    Add move state in player scene

---
+++ godot/games/jungle_gap/scripts/move.gd
@@ -0,0 +1,24 @@
+extends Node
+class_name JG_MoveState
+
+var velocity: Vector2
+
+export(int) var walk_speek
+
+export(NodePath) onready var player = get_node(player) as KinematicBody2D
+
+func move() -> void:
+	if player.is_attacking:
+		return
+		
+	velocity = get_direction() * walk_speek
+	if velocity != Vector2.ZERO:
+		 print(velocity)
+	player.move_and_slide(velocity)
+	
+func get_direction() -> Vector2:
+	return Vector2(
+		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
+		(Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")) / 2
+	)
+	#conferir se, divide por 2 o y no isometrico ou normaliza o total  
