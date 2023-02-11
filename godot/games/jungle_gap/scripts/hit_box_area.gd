6f6b883d0a00d413ddfcb2b0ad476ac70558b1c9
LordEtrius <marciomaia@gmail.com>
2023-02-11 11:13:08 -0300

    2 Players Controls in JG
    Add player1and player2 move and atack functions

--- godot/games/jungle_gap/scripts/hit_box_area.gd
+++ godot/games/jungle_gap/scripts/hit_box_area.gd
@@ -1,6 +1,17 @@
 extends Area2D
 class_name HitBoxArea
 
+export(NodePath) onready var player1 = get_node(player1) as KinematicBody2D
 
 func _on_HitboxArea_area_entered(area):
-	pass
+	var area_name = area.get_name()
+	print(area_name)
+	if area_name == "Fire":
+		if area.distancia_perc < 12:
+			player1.move_and_slide(area.direction * 7000)
+		elif area.distancia_perc > 12 and area.distancia_perc < 22:
+			player1.move_and_slide(area.direction * 5500)
+		elif area.distancia_perc > 22 and area.distancia_perc < 32:
+			player1.move_and_slide(area.direction * 3500)
+		else:
+			player1.move_and_slide(area.direction * 2000)
--- godot/games/jungle_gap/scripts/player.gd
+++ godot/games/jungle_gap/scripts/player.gd
@@ -13,8 +13,12 @@ var is_attacking: bool = false
 func _physics_process(_delta: float) -> void:
 	move_state.move()
 	attack_state.attack()
-	move_mira()	
+	#move_mira()	
+	move_mira_mouse()
 	
+func move_mira_mouse() -> void:
+	mira.position = get_local_mouse_position()
+		
 func move_mira() -> void:
 		var dir = Vector2(
 			Input.get_action_strength("mira_down") - Input.get_action_strength("mira_up"),
