79c294179b40a0112254d1c0ffeeb98a5f6787c9
rheidner <rheidner.achiles@gmail.com>
2023-02-13 15:07:17 -0300

    remove some `print_debug`

--- godot/maps/board/player/hand/Deck.gd
+++ godot/maps/board/player/hand/Deck.gd
@@ -18,12 +18,10 @@ func get_hand() -> Array:
 
 func reset_hand():
 	hand = [null, null, null, null, null]
-	print_debug("reset_hand", deck, hand)
 
 func add_card_to_deck(card: Card):
 	deck.append(card)
 	emit_signal("added_to_deck", card, deck.size() - 1)
-	print_debug("add_card_to_deck", deck, hand)
 
 func remove_card(card: Card):
 	var deck_idx = deck.find(card)
@@ -38,13 +36,11 @@ func remove_card_from_hand(hand_idx):
 		hand[hand_idx] = null
 		emit_signal("added_to_deck", card, deck.size() - 1)
 		emit_signal("removed_from_hand", card, hand_idx)
-	print_debug("remove_card_from_hand", deck, hand)
 
 func change_hand_order(from_idx, to_idx):
 	var aux = hand[from_idx]
 	hand[from_idx] = hand[to_idx]
 	hand[to_idx] = aux
-	print_debug("change_hand_order", deck, hand)
 
 func pick_card(card: Card, hand_idx: int):
 	if hand_idx < hand.size() and hand_idx >= 0:
@@ -54,4 +50,3 @@ func pick_card(card: Card, hand_idx: int):
 			hand[hand_idx] = card
 			emit_signal("added_to_hand", card, hand_idx)
 			emit_signal("removed_from_deck", card, deck_idx)
-	print_debug("pick_card", deck, hand)
