50ffbaa6b36b419855d36da31774f609efc3ee98
rheidner <rheidner.achiles@gmail.com>
2023-02-22 20:12:02 -0300

    fix some debugger errors

--- godot/maps/board/tile/arrow/Arrow.gd
+++ godot/maps/board/tile/arrow/Arrow.gd
@@ -10,12 +10,11 @@ func init(from_tile, to_tile):
 	self.from_tile = from_tile
 	self.to_tile = to_tile
 	
+	var direction = from_tile.position.direction_to(to_tile.position)
 	var angle = from_tile.position.angle_to_point(to_tile.position)
-	var x = from_tile.arrow_circle_radius * cos(angle + PI)
-	var y = from_tile.arrow_circle_radius * sin(angle + PI)
 	
-	self.rect_global_position += Vector2(x, y)
 	self.rect_rotation += rad2deg(angle + PI)
+	self.rect_position += direction * from_tile.arrow_circle_radius
 	
 	return self
 
