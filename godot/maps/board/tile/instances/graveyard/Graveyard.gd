09c46422018a8c23df105ef35b13053cc41a2a47
rheidner <rheidner.achiles@gmail.com>
2023-03-09 18:26:36 -0300

    format files

--- godot/maps/board/tile/instances/graveyard/Graveyard.gd
+++ godot/maps/board/tile/instances/graveyard/Graveyard.gd
@@ -1,15 +1,15 @@
 tool
 extends Tile
 
+
 func pre_turn_effect(board, player):
 	# restores player
 	yield(player.restore(), "completed")
-	CardEvent.emit_signal(
-		"record", "%s rises again" % player.nick
-	)
+	CardEvent.emit_signal("record", "%s rises again" % player.nick)
 	# move player out of graveyard
 	# (goes to next tile)
 	yield(player.move(), "completed")
+
 
 func _init():
 	._init()
