0721a3b6340edcd7da655f0b10c38e0c117f74bb
rheidner <rheidner.achiles@gmail.com>
2023-03-25 04:18:04 -0300

    feat: add arity and pool categories for cards
    also the get_random_card functions now operates considering the arity chance

--- godot/maps/board/cards/Collection.gd
+++ godot/maps/board/cards/Collection.gd
@@ -1,12 +1,38 @@
 extends Node
 
-var cards = [Swap, TeleportToTile, Teleport, WhereAmI, Dice, SubPoint, Suicide]
+var _chances_base := {
+	basic = 45,
+	rare = 80,
+	broken = 95,
+	legendary = 100
+}
 
-func get_random_card():
-	var cards_size = self.cards.size()
-	var idx := 0
-	if cards_size > 0:
-		idx = Util.randi_from_range(0, cards.size() - 1)
-	var card_type = cards[idx]
-	var card = card_type.new()
-	return card
+var cards = {
+#	water = {
+#		basic = [Dice],
+#		rare = [Dice],
+#		broken = [Dice],
+#		legendary = [Dice],
+#	},
+	any = {
+		basic = [Dice, SubPoint, WhereAmI],
+		rare = [Teleport, Suicide],
+		broken = [TeleportToTile],
+		legendary = [Swap],
+	}
+}
+
+func get_random_card(pools := [] as Array[String], chances := _chances_base):
+	pools.append("any")
+	var chance := randi() % 100
+	for arity in chances.keys():
+		if chance < chances[arity]:
+			var cards = _filter_cards_by_arity(pools, arity) as Array[Card]
+			var card_type = Util.array_get_random(cards)
+			return card_type.new() as Card
+
+
+func _filter_cards_by_arity(pools: Array[String], arity: String) -> Array:
+	return pools.map(
+		func(pool): return cards[pool][arity]
+	).reduce(func(a, b): return a + b, [])
