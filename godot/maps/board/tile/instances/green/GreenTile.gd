c966b657f5fb6ee2474871a86803912ddd7a24fb
LordEtrius <59979253+LordEtrius@users.noreply.github.com>
2023-03-28 09:20:48 -0300

    Update GreenTile.gd

--- godot/maps/board/tile/instances/green/GreenTile.gd
+++ godot/maps/board/tile/instances/green/GreenTile.gd
@@ -4,5 +4,5 @@ extends Tile
 func effect(board, player):
 	#player.hp -= player.hp * 0.25
 	player.hp -= 8
-	TileEvent.emit_signal("record", "Blue tile: %s Loss HP" % player.nick)
+	TileEvent.emit_signal("record", "Green tile: %s Loss HP" % player.nick)
 	await player.animate_scale()
